2be00cbcdf148768df5d36a46917843414f1abba
Revathiraji1 <revaanand26@gmail.com>
2022-01-03 14:27:00 +0530

    Revathi has done

--- src/org/tcs/login/Desktop.java
+++ src/org/tcs/login/Desktop.java
@@ -21,11 +21,17 @@ public class Desktop implements Hardware, Software {
 
 	}
 
+	private void add() {
+		System.out.println("add");
+	}
+
 	public static void main(String[] args) {
 		Desktop d = new Desktop();
 		d.desktopModel();
 		d.hardwareResourses();
 		d.softwareResources();
 		d.sub();
+		d.add();
+		
 	}
 }
