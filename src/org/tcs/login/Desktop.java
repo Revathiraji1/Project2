ea3022288a3c10653a952b6b169dc7d30aa47b98
Revathiraji1 <revaanand26@gmail.com>
2022-01-03 14:18:14 +0530

    Raji has done

--- src/org/tcs/login/Desktop.java
+++ src/org/tcs/login/Desktop.java
@@ -1,24 +1,31 @@
 package org.tcs.login;
 
-public class Desktop implements Hardware,Software{
+public class Desktop implements Hardware, Software {
 
 	private void desktopModel() {
-     System.out.println("Apple 11");
+		System.out.println("Apple 11");
 	}
 
 	@Override
 	public void softwareResources() {
-System.out.println("Software");		
+		System.out.println("Software");
 	}
 
 	@Override
 	public void hardwareResourses() {
-System.out.println("Hardware");		
+		System.out.println("Hardware");
 	}
+
+	private void sub() {
+		System.out.println("sub");
+
+	}
+
 	public static void main(String[] args) {
 		Desktop d = new Desktop();
 		d.desktopModel();
 		d.hardwareResourses();
 		d.softwareResources();
+		d.sub();
 	}
 }
